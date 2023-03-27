7545e89b9718cd818c839239e1255f4809063359
Nikita Romanovich <nikitarom@hotmail.com>
2023-03-27 20:32:09 +0300

    Create lcd.h

---
+++ lcd.h
@@ -0,0 +1,20 @@
+#ifndef LCD_H
+#define LCD_H
+#include "stm8s.h"
+#include "main.h"
+//#include "I2C.h"
+
+#define rs GPIOD->ODR//выход RB2//
+#define e GPIOD->ODR//выходRB3//
+//--------------------------------------------------------------
+//#include <xc.h>
+//--------------------------------------------------------------
+void LCD_PORT_init();
+void LCD_Init(void);
+//void LCD_String(char* st);
+void LCD_SetPos(unsigned char x, unsigned char y);
+void sendbyte(unsigned char c, unsigned char mode);
+void sendbyteHalf(unsigned char c);
+void LCD_Clear();
+//--------------------------------------------------------------
+#endif /* LCD_H */
