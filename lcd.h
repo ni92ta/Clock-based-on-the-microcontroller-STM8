99e3aeb05a0b14336ee84e0bf2e2a4c4bcd35ec0
Nikita Romanovich <nikitarom@hotmail.com>
2023-03-29 20:05:47 +0300

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -4,17 +4,17 @@
 #include "main.h"
 //#include "I2C.h"
 
-#define rs GPIOD->ODR//выход RB2//
-#define e GPIOD->ODR//выходRB3//
+//#define rs GPIOD->ODR//выход RB2//
+//#define e GPIOD->ODR//выходRB3//
 //--------------------------------------------------------------
 //#include <xc.h>
 //--------------------------------------------------------------
-void LCD_PORT_init();
+//void LCD_PORT_init(void);
 void LCD_Init(void);
 //void LCD_String(char* st);
 void LCD_SetPos(unsigned char x, unsigned char y);
 void sendbyte(unsigned char c, unsigned char mode);
 void sendbyteHalf(unsigned char c);
-void LCD_Clear();
+void LCD_Clear(void);
 //--------------------------------------------------------------
 #endif /* LCD_H */
