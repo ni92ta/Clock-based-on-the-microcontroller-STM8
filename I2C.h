46898a39c656ab05936cdc836522ea5748bf2917
Nikita Romanovich <nikitarom@hotmail.com>
2023-04-09 15:57:50 +0300

    Update I2C.h

--- I2C.h
+++ I2C.h
@@ -1,13 +1,12 @@
 #ifndef I2C_H
 #define	I2C_H
 #include "stm8s.h"
-#define SCL GPIOB->DDR // |= (1<<4)
-#define SDA GPIOB->DDR  //TRISC4 вход  |= (1<<5)
-#define SCL_IN GPIOB->IDR & (1 << 4) //&= ~(1<<4)//выход
-#define SDA_IN GPIOB->IDR & (1 << 5) //&= ~(1<<5)
-//#define _XTAL_FREQ 3579000
-#define dev_addrw 0b11010000 //запись 0b10100000
-#define dev_addrr 0b11010001 //чтение 0b10100001
+#define SCL GPIOB->DDR 
+#define SDA GPIOB->DDR 
+#define SCL_IN GPIOB->IDR & (1 << 4) 
+#define SDA_IN GPIOB->IDR & (1 << 5)
+#define dev_addrw 0b11010000
+#define dev_addrr 0b11010001
 //----------------------------------------
 void i2c_start(void);
 void i2c_stop (void);
