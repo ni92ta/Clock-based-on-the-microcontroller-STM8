bdfe4a221427357842a04e70522821a0be2d01e0
Nikita Romanovich <nikitarom@hotmail.com>
2023-04-09 15:58:02 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,21 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-//--------------------------------------------------------------
-//#define _XTAL_FREQ 3579000
-//#include <xc.h>
-
-
-//--------------------------------------------------------------
+//----------------------------------------------------------
 #include "lcd.h"
 #include "I2C.h"
 #include "stm8s.h"
-//#include "void_function.h"
 void delay_ms(unsigned int set_ms);
+void delay_us(unsigned int set_us);
 void LCD_Init();
-//void Day_Switch (void);
-//void lcd_mask (unsigned char mask);
- //unsigned char DAY_1 = 0b10101000;//ПН
- //unsigned char DAY_2 = 0b01001000;//ВТ
- //unsigned char Weekdays;//переменная дня недели до преобразования
-//--------------------------------------------------------------
+//----------------------------------------------------------
 #endif /* MAIN_H */
